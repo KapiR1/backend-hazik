dedad6e3272fb133c37e1f45ab9617fa3f13a254
KDTL <KDTL@DESKTOP-6TN4NRL>
2024-10-09 17:44:28 +0200

    Add project files.

---
+++ WCF_elso_server/Controllers/ZeneszamController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WCF_elso_server.Models;
+using System.IO;
+
+namespace WCF_elso_server.Controllers
+{
+    public class ZeneszamController
+    {
+        public List<Zeneszam> ZeneszamokLista()
+        {
+            string[] sor = File.ReadAllLines("C:\\Users\\Admin\\Documents\\Suli\\Órákra_Backend\\13B\\02alkalom\\WCF_elso\\WCF_elso_server\\ZeneszamAdatok.txt");
+            List<Zeneszam> lista = new List<Zeneszam>();
+            for (int i = 1; i < sor.Length; i++)
+            {
+                string[] valtas = sor[i].Split(';');
+                lista.Add(new Zeneszam()
+                {
+                    ZeneszamAz = int.Parse(valtas[0]),
+                    ZeneszamCim = valtas[1],
+                    ZeneszamHossz = int.Parse(valtas[2])
+                });
+            }
+            return lista;
+        }
+        public string InsertZeneszam(Zeneszam ujZeneszam)
+        {
+            ujZeneszam.ZeneszamAz = GenerateID();
+            StreamWriter kimenet = new StreamWriter("C:\\Users\\Admin\\Documents\\Suli\\Órákra_Backend\\13B\\02alkalom\\WCF_elso\\WCF_elso_server\\ZeneszamAdatok.txt", true);
+
+            kimenet.WriteLine(ujZeneszam.ToString());
+            kimenet.WriteLine(ujZeneszam.ZeneszamHossz.ToString());
+            kimenet.Close();
+            return "Sikeresen mentettük az Zeneszamot.";
+        }
+        int GenerateID()
+        {
+            return ZeneszamokLista().Select(Zeneszam => Zeneszam.ZeneszamAz).ToList().Max() + 1;
+        }
+
+
+        public string UpdateZeneszam(Zeneszam zeneszam)
+        {
+            List<Zeneszam> aktualisok = ZeneszamokLista();
+            int index = 0;
+            while (index < aktualisok.Count &&
+                aktualisok[index].ZeneszamAz != zeneszam.ZeneszamAz)
+            {
+                index++;
+            }
+
+            if (index < aktualisok.Count)
+            {
+                aktualisok[index].ZeneszamCim = zeneszam.ZeneszamCim;
+                aktualisok[index].ZeneszamHossz = zeneszam.ZeneszamHossz;
+                StreamWriter ujAllomany = new StreamWriter("C:\\Users\\Admin\\Documents\\Suli\\Órákra_Backend\\13B\\02alkalom\\WCF_elso\\WCF_elso_server\\ZeneszamAdatok.txt");
+                ujAllomany.WriteLine("ZeneszamAzon;ZeneszamNev;ZeneszamHossz");
+                foreach (Zeneszam a in aktualisok)
+                {
+                    ujAllomany.WriteLine(a.ToString());
+                }
+                ujAllomany.Close();
+                return "A módosítás sikeres";
+
+            }
+            else
+                return "Nincs ilyen azonosítójú zeneszam";
+        }
+
+        public string TorolZeneszam(int id)
+        {
+            List<Zeneszam> aktualisok = ZeneszamokLista();
+            int index = 0;
+            while (index < aktualisok.Count &&
+                aktualisok[index].ZeneszamAz != id)
+            {
+                index++;
+            }
+
+            if (index < aktualisok.Count)
+            {  
+                StreamWriter ujAllomany = new StreamWriter("C:\\Users\\Admin\\Documents\\Suli\\Órákra_Backend\\13B\\02alkalom\\WCF_elso\\WCF_elso_server\\ZeneszamAdatok.txt");
+                ujAllomany.WriteLine("ZeneszamAzon;ZeneszamNev;ZeneszamHossz");
+                foreach (Zeneszam a in aktualisok)
+                {
+                    ujAllomany.WriteLine(a.ToString());
+                }
+                ujAllomany.Close();
+                return "A törlés sikeres";
+            }
+            else
+                return "Nincs ilyen azonosítójú előadó";
+        }
+    }
+}
---
+++ WCF_elso_server/Service1.svc.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using WCF_elso_server.Controllers;
+using WCF_elso_server.Models;
+
+namespace WCF_elso_server
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public List<Eloado> GetEloado()
+        {
+            List<Eloado> list = new EloadoController().ELoadokLista();
+            return list;
+        }
+
+        public List<Zeneszam> GetZeneszams()
+        {
+            List<Zeneszam> lista = new ZeneszamController().ZeneszamokLista();
+            return lista;
+        }
+
+        public string GetEloadoName()
+        {
+            Eloado eloado = new Eloado()
+            {
+                EloadoAz = 1,
+                EloadoName = "Queen"
+            };
+            return eloado.EloadoName;
+        }
+
+        public string GetZeneszam()
+        {
+            Zeneszam zeneszam = new Zeneszam()
+            {
+                ZeneszamAz = 1,
+                ZeneszamCim = "Rain",
+                ZeneszamHossz = 213
+            };
+            return zeneszam.ZeneszamCim;
+        }
+
+        public string ModositEloado(Eloado eloado)
+        {
+            return new EloadoController().UpdateEloado(eloado);
+        }
+
+        public string UjEloado(Eloado eloado)
+        {           
+            return new EloadoController().InsertEloado(eloado);
+        }
+
+        public string TorolEloado(int id)
+        {
+            return "";
+        }
+
+        public string ModositZeneszam(Zeneszam zeneszam)
+        {
+            return new ZeneszamController().UpdateZeneszam(zeneszam);
+        }
+
+        public string UjZeneszam(Zeneszam zeneszam)
+        {
+            return new ZeneszamController().InsertZeneszam(zeneszam);
+        }
+
+        public string TorolZeneszam(int id)
+        {
+            return "";
+        }
+    }
+}
